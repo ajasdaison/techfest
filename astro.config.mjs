396ee958fcaeff0c32b9cd5a3ad5f55464582eca
Ajas Daison <ajas.daison@gmail.com>
2026-01-22 15:11:52 +0530

    Vercel SSR setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import 'dotenv/config';
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
 
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   vite: {
     server: {
       allowedHosts: ['.ngrok-free.app'],
